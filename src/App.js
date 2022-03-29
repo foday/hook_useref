2f71ea2fef5698eef781e6571d3114102ebc2ec0
foday <foday_s@yahoo.com>
2022-03-29 10:47:14 -0500

    Referencing elements inside of HTML using Ref attribute and useRef, useState should manage your state not useRef
    Create a ref for our input: const inputRef = useRef(), come inside of the <input & put ref={inputRef}> so now whenever our input ref gets rendered on the screen it's going to set this input ref variable equal to the document element: '<input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />' this input. Create Focus <button> to put focus on the <input> box when <button> is clicked, create a function

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import './App.css';
 import React, {useState, useRef, useEffect} from 'react';
 
 function App() {
-  const [name, setName] = useState("sim");
-  const renderCount = useRef(0);
-
-  useEffect(() => {
-    renderCount.current = renderCount.current + 1 
-  })
+  const [name, setName] = useState("foday");
+  // const renderCount = useRef(0);
+  const inputRef = useRef();
+  // useEffect(() => {
+  //   renderCount.current = renderCount.current + 1 
+  // })
+  function focus() {
+    inputRef.current.focus()
+  }
 
   return (
     <div className="App">
-      <input value={name} onChange={e => setName(e.target.value)} ></input>
+      {/* <input value={name} onChange={e => setName(e.target.value)} ></input> */}
+      <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} ></input>
       <div>My Name is {name}</div>
-      <div>rendered: {renderCount.current} times</div>
+      <button onClick={focus}>Focus</button>
+      {/* <div>rendered: {renderCount.current} times</div> */}
     </div>
   );
 }
